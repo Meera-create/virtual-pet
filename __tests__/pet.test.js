709d618e038b2d89b6a6fc89b37be8b01301bf13
Meera A <mirunaalini.alagarajah@gmail.com>
2023-07-05 14:18:10 +0100

    getting older

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -14,3 +14,18 @@ describe('constructor',()=>{
         expect(pet.name).toEqual('Fido');
     })
 })
+
+describe('constructor',()=>{
+    it('has an initial age of 0',()=>{
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    })
+})
+
+describe('growUp',()=>{
+    it('increments the age by 1',()=>{
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    })
+})
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,11 @@
 function Pet(name) {
     this.name=name;
+    this.age=0;
+    
+}
+
+Pet.prototype.growUp=function(){
+    this.age+=1;
 }
 
 
